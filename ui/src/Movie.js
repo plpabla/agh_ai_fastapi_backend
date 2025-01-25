58f94bbe170be54291ed459eaf0e69ea7f706078
pawel-laptop <p.blaszczy@gmail.com>
2025-01-25 18:02:00 +0100

    display movies in accordion

--- ui/src/Movie.js
+++ ui/src/Movie.js
@@ -1,10 +1,18 @@
-export default function Movie({ title, year, deleteMovie }) {
+import Accordion from "react-bootstrap/Accordion";
+
+export default function Movie({ movie }) {
   return (
     <>
-      {title} ({year}){" "}
-      <button className="button button-clear" onClick={deleteMovie}>
-        Delete
-      </button>
+      <Accordion.Header>
+        {movie.title} ({movie.year})
+      </Accordion.Header>
+      <Accordion.Body>
+        <p>Director: {movie.director}</p>
+        <p>{movie.description}</p>
+        <button className="button button-clear" onClick={movie.deleteMovie}>
+          Delete
+        </button>
+      </Accordion.Body>
     </>
   );
 }
--- ui/src/MoviesList.js
+++ ui/src/MoviesList.js
@@ -1,53 +1,14 @@
-import Movie from "./Movie";
 import Accordion from "react-bootstrap/Accordion";
+import Movie from "./Movie";
 
 export default function MoviesList({ movies, setMovies, deleteMovie }) {
   return (
     <Accordion>
-      <Accordion.Item eventKey="0">
-        <Accordion.Header>Accordion Item #1</Accordion.Header>
-        <Accordion.Body>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </Accordion.Body>
-      </Accordion.Item>
-      <Accordion.Item eventKey="1">
-        <Accordion.Header>Accordion Item #2</Accordion.Header>
-        <Accordion.Body>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </Accordion.Body>
-      </Accordion.Item>
-    </Accordion>
-  );
-}
-
-function MoviesList2({ movies, setMovies, deleteMovie }) {
-  return (
-    <ul>
       {movies.map((movie, index) => (
-        <li key={index}>
-          <Movie
-            {...movie}
-            deleteMovie={() => {
-              console.log(">>> deleting movie", movie.id);
-              deleteMovie(movie.id).then(() =>
-                setMovies(movies.filter((_, i) => i !== index))
-              );
-            }}
-          />
-        </li>
+        <Accordion.Item eventKey={index} key={index}>
+          <Movie movie={movie} />
+        </Accordion.Item>
       ))}
-    </ul>
+    </Accordion>
   );
 }
